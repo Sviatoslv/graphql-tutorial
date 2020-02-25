c6382afb3c3dfaa6762e2ddd807633ce7074f500
sviatoslv <sviatoslav.boguslavskyi@gmail.com>
2020-02-25 15:11:40 +0200

    Mongoos added, models for mongoose added

--- server/server/app.js
+++ server/server/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/shema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect(
+  'mongodb+srv://user:pass1234@cluster0-isxm8.mongodb.net/graph_ql?retryWrites=true&w=majority',
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
 
-app.listen(PORT, err => {
-  err ? console.log(err) : console.log('Server started!');
-});
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection Error ${err}`));
+dbConnection.once('open', () => console.log('Connection Successfull'));
+
+app.listen(PORT, err => console.log(err ? err : 'Server Starts'));
